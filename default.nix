c28e6c8f209efcf9a7997ad0ee967beb37c22205
Shochraos <github@raiton.net>
2026-06-03 11:59:47 +0200

    added proton name changing to a more readable alternative

--- default.nix
+++ default.nix
@@ -1,13 +1,17 @@
-{ lib
-, stdenv
-, fetchurl
-, zstd
-, protonCachyosVersions
+{
+  lib,
+  stdenv,
+  fetchurl,
+  zstd,
+  protonCachyosVersions,
 }:
 
-stdenv.mkDerivation {
-  name = "proton-cachyos-slr";
+stdenv.mkDerivation rec {
+  pname = "proton-cachyos-slr";
   version = "${protonCachyosVersions.base}-${protonCachyosVersions.release}";
+
+  archiveName = "proton-cachyos-${version}-slr-x86_64";
+  protonDisplayName = "Proton-CachyOS-${version}";
 
   src = fetchurl {
     url = "https://mirror.cachyos.org/repo/x86_64/cachyos/proton-cachyos-slr-1:${protonCachyosVersions.base}.${protonCachyosVersions.release}-1-x86_64.pkg.tar.zst";
@@ -16,8 +20,16 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [ zstd ];
 
-  installPhase = ''
+  unpackPhase = ''
     tar -I zstd -xf $src
+  '';
+
+  postPatch = ''
+    substituteInPlace "usr/share/steam/compatibilitytools.d/proton-cachyos-slr/compatibilitytool.vdf" \
+      --replace-fail "$archiveName" "${protonDisplayName}"
+  '';
+
+  installPhase = ''
     mkdir -p $out/share/steam/compatibilitytools.d
     mv usr/share/steam/compatibilitytools.d/proton-cachyos-slr $out/share/steam/compatibilitytools.d/
   '';
@@ -27,6 +39,6 @@ stdenv.mkDerivation {
     homepage = "https://github.com/CachyOS/proton-cachyos";
     license = licenses.bsd3;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ kimjongbing ];
+    maintainers = with maintainers; [ shochraos ];
   };
 }
